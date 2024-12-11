a315ec56c10064276e859c1614dd6adee63c4b59
root <root@Didi.localdomain>
2024-12-11 13:14:20 +0100

    First test for selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,15 +1,63 @@
-#include "sort.h"
+#include <sort.h>
 
 /**
- * selection_sort - 
- * @array:
- * @size:
- * Description:
- */
+* swap - swaps two elements in an array
+* @a: first element
+* @b: second element
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
 
+	*a = *b;
+	*b = temp;
+}
+
+/**
+* selection_sort - sorts an array using the Selection sort algorithm
+* @array: pointer to the array
+* @size: size of the array
+*/
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
 
 
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
 
+
+		if (min_idx != i)
+		{
+			swap(&array[i], &array[min_idx]);
+
+			print_array(array, size);
+		}
+	}
 }
+
+/**
+* print_array - prints an array of integers
+* @array: pointer to the array
+* @size: size of the array
+*/
+void print_array(int *array, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
