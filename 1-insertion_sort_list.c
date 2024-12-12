58377963be85f9365997aba6e6f7410ff251b7a0
jbn179 <9772@holbertonstudents.com>
2024-12-12 11:27:39 +0100

    add task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,31 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *sorted, *temp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
+	sorted = *list;
+	current = sorted->next;
 
+	while (current != NULL)
+	{
+		temp = current->next;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+		current = temp;
+	}
 }
